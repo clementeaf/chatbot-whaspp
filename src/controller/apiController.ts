bb338edd83824e7365bed69c314358ec44da3151
clemente91 <clemente.arriagada@tigerbytes.de>
2024-10-21 14:15:49 -0300

    [Update] verify

--- src/controller/apiController.ts
+++ src/controller/apiController.ts
@@ -1,5 +1,17 @@
-export const verificar = (req: any, res: { send: (arg0: string) => void }) => {
-    res.send('Verificado');
+export const verificar = (req: any, res: {
+    [x: string]: any; send: (arg0: string) => void 
+}) => {
+    try {
+        const tokenClemente = "CLEMENTEFALCONE";
+        const token = req.query["hub.verify_token"];
+        const chanllenge = req.query["hub.challenge"];
+
+        res.send(chanllenge);
+        
+        console.log(req);
+    } catch (error) {
+        res.status(400).send();
+    }
 }
 
 export const recibir = (req: any, res: { send: (arg0: string) => void }) => {
